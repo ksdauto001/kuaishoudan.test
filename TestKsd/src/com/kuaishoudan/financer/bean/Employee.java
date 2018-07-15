0f37a28b3d031c6b4b5e05e36e4264b9748be73e
liuhl0@126.com <liuhl0@126.com>
2018-07-15 13:07:55 +0800

    11

---
+++ TestKsd/src/com/kuaishoudan/financer/bean/Employee.java
@@ -0,0 +1,32 @@
+package com.kuaishoudan.financer.bean;
+
+public class Employee {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	String username;
+	String  account;
+	String desc;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+}
